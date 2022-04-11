9e12bb646ed0fe3d6082d2d504b0094a167b5cf5
CJHydraGenZ <49280905+CJHydraGenZ@users.noreply.github.com>
2022-04-11 01:29:32 +0000

    Deploying to gh-pages from @ CJHydraGenZ/portfolio@28c00668cbcbd10cfa8fb86fe8daa5e9b56a1f7d 🚀

---
+++ _next/static/9OcF0ZuT1rK8s3P4eQSHq/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/247-2e74a7e1f868c02f.js","static/chunks/pages/index-161b73f654771bee.js"],"/_error":["static/chunks/pages/_error-1995526792b513b2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
